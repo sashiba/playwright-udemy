fcd5eec571138646bf1cc616e04cf09f569d1c6b
Aleksandar Pirnarev <aleksandarpirnarev@coherentsolutions.com>
2025-04-10 11:38:34 +0300

    add anotated test

--- src/test/java/org/sashiba/ASimplePlaywrightTest.java
+++ src/test/java/org/sashiba/ASimplePlaywrightTest.java
@@ -1,48 +1,35 @@
 package org.sashiba;
 
-import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.junit.Options;
-import com.microsoft.playwright.junit.OptionsFactory;
-import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-@UsePlaywright(ASimplePlaywrightTest.CustomOptions.class)
 public class ASimplePlaywrightTest {
     private static final String URL = "https://practicesoftwaretesting.com";
 
-    public static class CustomOptions implements OptionsFactory {
+    private Playwright playwright;
+    private Browser browser;
+    private Page page;
 
-        @Override
-        public Options getOptions() {
-            return new Options()
-                    .setHeadless(false)
-                    .setLaunchOptions(new BrowserType.LaunchOptions().setArgs(List.of("--no-sandbox", "--disable-gpu", "--disable-extensions")));
-        }
+    @BeforeEach
+    void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+        page = browser.newPage();
     }
 
-//    @UsePlaywright is doing setup() and tearDown(), we need to pass Page as parameter in tests
-//    private Playwright playwright;
-//    private Browser browser;
-//    private Page page;
-//    @BeforeEach
-//    void setup() {
-//        playwright = Playwright.create();
-//        browser = playwright.chromium().launch();
-//        page = browser.newPage();
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        browser.close();
-//        playwright.close();
-//    }
+    @AfterEach
+    void tearDown() {
+        browser.close();
+        playwright.close();
+    }
 
     @Test
-    void pageTitleTest(Page page) {
+    void pageTitleTest() {
         page.navigate(URL);
 
         String title = page.title();
@@ -50,7 +37,7 @@ public class ASimplePlaywrightTest {
     }
 
     @Test
-    void searchFieldTest(Page page) {
+    void searchFieldTest() {
         page.navigate(URL);
 
         page.locator("#search-query").fill("Pliers");
---
+++ src/test/java/org/sashiba/AnAnnotatedPlaywrightTest.java
@@ -0,0 +1,45 @@
+package org.sashiba;
+
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+@UsePlaywright(ASimplePlaywrightTest.CustomOptions.class)
+public class AnAnnotatedPlaywrightTest {
+    private static final String URL = "https://practicesoftwaretesting.com";
+
+    public static class CustomOptions implements OptionsFactory {
+
+        @Override
+        public Options getOptions() {
+            return new Options()
+                    .setHeadless(false)
+                    .setLaunchOptions(new BrowserType.LaunchOptions().setArgs(List.of("--no-sandbox", "--disable-gpu", "--disable-extensions")));
+        }
+    }
+
+    @Test
+    void pageTitleTest(Page page) {
+        page.navigate(URL);
+
+        String title = page.title();
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+    }
+
+    @Test
+    void searchFieldTest(Page page) {
+        page.navigate(URL);
+
+        page.locator("#search-query").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+        int matchingResults = page.locator(".card").count();
+
+        Assertions.assertTrue(matchingResults > 0);
+    }
+}
